7226b6a8f11d1f2d7cbd2ad9c8464e8119f815c1
alvinastr <119154719+alvinastr@users.noreply.github.com>
2024-07-27 00:14:03 +0700

    LMS

--- frontend/src/components/Teacher/TeacherRegister.js
+++ frontend/src/components/Teacher/TeacherRegister.js
@@ -1,7 +1,25 @@
-import {Link} from 'react-router-dom';
-import {useEffect} from 'react';
-
+// import {Link} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+// import axios from 'axios';
 function TeacherRegister(){
+    const [teacherData, setteacherData]=useState(null);
+    setteacherData({
+        'full_name': '',
+        'email': '',
+        'password': '',
+        'qualification': '',
+        'mobile_no': '',
+        'skills': '',
+        'status': '',
+    });
+    // Change Element Value
+    const handleChange=(event)=>{
+        setteacherData({
+           ...teacherData,
+           [event.target.name]: event.target.value
+        });
+    }
+    console.log();
     useEffect(() => {
         document.title = "Teacher Register"
     });
@@ -15,35 +33,28 @@ function TeacherRegister(){
                             <form>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail" className="form-label">Full Name</label>
-                                    <input type="text" className="form-control"/>
+                                    <input onChange={handleChange} name="full_name" type="text" className="form-control"/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail" className="form-label">Email</label>
-                                    <input type="email" className="form-control"/>
+                                    <input onChange={handleChange} name="email" type="email" className="form-control"/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                                    <input onChange={handleChange} name="password" type="password" className="form-control" id="exampleInputPassword1"/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail" className="form-label">Qualification</label>
-                                    <input type="text" className="form-control"/>
+                                    <input onChange={handleChange} type="text" name="qualification" className="form-control"/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail" className="form-label">Mobile NO</label>
-                                    <input type="number" className="form-control"/>
+                                    <input onChange={handleChange} name="mobile_no" type="number" className="form-control"/>
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="exampleInputEmail" className="form-label">Skills</label>
-                                    <textarea className="form-control"/>
+                                    <textarea onChange={handleChange} name="skills" className="form-control"/>
                                     <div id="emailHelp" className="form-text">Python, PHP, Java, JavaScript, etc</div>
-                                </div>
-                                <div className="mb-3">
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" id="exampleCheck1"/>
-                                        <label className="form-check-label" htmlFor="disabledFieldsetCheck">Remember
-                                            Me</label>
-                                    </div>
                                 </div>
                                 <button type="submit" className="btn btn-primary">Submit</button>
                             </form>
